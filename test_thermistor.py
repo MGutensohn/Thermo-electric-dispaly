8d396c9afb074976774d823035f6773ffcdc807d
Michael Gutensohn <michael@gutensohn.me>
2018-11-19 18:07:56 -0800

    Debugging analog input

--- test_thermistor.py
+++ test_thermistor.py
@@ -11,8 +11,8 @@ def read_celsius(adc_channel=0):
         cmd = 0b01100000
     else:
         cmd = 0b01110000
-    reply_bytes = spi.xfer2([1, cmd, 0])
-    reply = ((reply_bytes[1] & 3) << 8) + reply_bytes[2]
+    reply_bytes = spi.xfer2([cmd, 0])
+    reply = ((reply_bytes[0] & 3) << 8) + reply_bytes[1]
     print "Channel " + str(adc_channel) + ": " + str(reply_bytes) + " reply: " + str(reply)
 
     volts = (reply * 3.3) / 1024 #calculate the voltage
