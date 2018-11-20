560191e9d31ccbec5ae530f06d150ad4f5d75f26
Michael Gutensohn <michael@gutensohn.me>
2018-11-19 17:12:35 -0800

    Testing temperature thresholding

--- test_thermistor.py
+++ test_thermistor.py
@@ -10,8 +10,11 @@ def read_celsius(adc_channel=0):
     cmd = 128
     if adc_channel:
         cmd += 32
-    reply_bytes = spi.xfer2([1, cmd, 0])
-    reply = ((reply_bytes[1] & 3) << 8) + reply_bytes[2]
+    reply_bytes = spi.xfer2([cmd, 0, 0])
+    # reply = ((reply_bytes[1] & 3) << 8) + reply_bytes[2]
+    reply = (reply_bytes[0] & 0x01) << 9
+    reply |= (reply_bytes[1] & 0xFF) << 1
+    reply |= (reply_bytes[2] & 0x80) >> 7
     print str(reply_bytes)
     print str(reply)
 
